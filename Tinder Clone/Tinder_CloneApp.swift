e3216e01c3c3148fe8f937ab3b5844f344f7f71f
Amish Tufail <amish.tufail2002@gmail.com>
2023-04-29 15:25:39 +0500

    Initial Commit

---
+++ Tinder Clone/Tinder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_CloneApp.swift
+//  Tinder Clone
+//
+//  Created by Amish Tufail on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
